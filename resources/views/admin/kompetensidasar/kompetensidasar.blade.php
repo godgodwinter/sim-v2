641cc4875761692d1dc92df30026cc5ced134790
kakadlz <chibi.m4ru@gmail.com>
2021-10-05 00:39:22 +0700

    update layout table

--- resources/views/admin/kompetensidasar/kompetensidasar.blade.php
+++ resources/views/admin/kompetensidasar/kompetensidasar.blade.php
@@ -268,10 +268,7 @@ $totalrow=$jmlkd+$jmlmateriperkd+1;
     <div class="row mt-sm-4">
 
         <div class="col-12 col-md-12 col-lg-12">
-            <div class="card profile-widget">
-                <div class="profile-widget-header">
-                    <img alt="image" src="{{ asset("assets/") }}/img/products/product-3-50.png" class="rounded-circle profile-widget-picture">
-                    <div class="profile-widget-items">
+            <div class="card">
                         <div class="form-group col-md-12 col-12 mt-1 text-right">
 
                             <button type="button" class="btn btn-icon btn-success btn-sm" data-toggle="modal" data-target="#add">
@@ -287,15 +284,14 @@ $totalrow=$jmlkd+$jmlmateriperkd+1;
                           </button>
                           <a href="/admin//export" type="submit" value="Import" class="btn btn-icon btn-primary btn-sm"><span
                                 class="pcoded-micon"> <i class="fas fa-download"></i> Export </span></a>
-                          </div>
-                    </div>
+
                 </div>
             {{-- @yield('datatable') --}}
             {{-- {{ dd($datas) }} --}}
 
                 <div class="card-body -mt-5">
                     <div class="table-responsive">
-                        <table class="table table-bordered table-md" style="border-">
+                        <table class="table table-sm table-striped" >
                         <tr>
                             @yield('headtable')
                         </tr>
